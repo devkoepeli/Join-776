07c13e8bf35fe45e9feea1f94199799660075f92
Makedonka Sander <makedonka28@gmail.com>
2023-11-16 20:05:09 +0100

    contacts

--- js/storage.js
+++ js/storage.js
@@ -2,6 +2,8 @@ const STORAGE_TOKEN = 'K2CAGMPAT75GP3D7DEFZIJMLYWBK83642E8S4PYO';
 const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
 
 
+
+
 async function setItem(key, value) {
     const payload = { key, value, token: STORAGE_TOKEN };
     return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload) })
@@ -13,3 +15,4 @@ async function getItem(key) {
     const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
     return fetch(url).then(res => res.json());
 }
+
