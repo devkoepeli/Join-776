673a70c5068af8013948c3441dac5c1fe15fc0d4
devkoepeli <devrim_kopeli@hotmail.com>
2023-11-20 21:24:34 +0100

    fetch task data

--- js/storage.js
+++ js/storage.js
@@ -2,8 +2,6 @@ const STORAGE_TOKEN = 'K2CAGMPAT75GP3D7DEFZIJMLYWBK83642E8S4PYO';
 const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
 
 
-
-
 async function setItem(key, value) {
     const payload = { key, value, token: STORAGE_TOKEN };
     return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload) })
@@ -13,6 +11,5 @@ async function setItem(key, value) {
 
 async function getItem(key) {
     const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
-    return fetch(url).then(res => res.json());
+    return fetch(url).then(res => res.json()).then(res => res.data.value);
 }
-
