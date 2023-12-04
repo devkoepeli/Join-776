ec144235b40d3bc85ea13de3609b8a51a57a8b16
Makedonka Sander <makedonka28@gmail.com>
2023-12-04 14:15:25 +0100

    storage functionen

--- js/storage.js
+++ js/storage.js
@@ -2,13 +2,23 @@ const STORAGE_TOKEN = 'K2CAGMPAT75GP3D7DEFZIJMLYWBK83642E8S4PYO';
 const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
 
 
+/**
+ * Uploads data into the backend.
+ * @param {key} key - data name (key)
+ * @param {array} value - data array to upload
+ * @returns - promise
+ */
 async function setItem(key, value) {
     const payload = { key, value, token: STORAGE_TOKEN };
     return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload) })
     .then(res => res.json());
 }
 
-
+/**
+ * Fetches data from the backend.
+ * @param {key} key - key name to fetch
+ * @returns - promise + JSON
+ */
 async function getItem(key) {
     const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
     return fetch(url).then(res => res.json()).then(res => res.data.value);
